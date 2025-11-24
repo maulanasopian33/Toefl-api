adc95283973c8a1a2a4f1e668203842dd311dc73
Maulana Sopian <maulana@antmediahost.com>
2025-11-24 09:48:03 +0700

    :sparkles: role change

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const checkAuth = require('../middlewares/authMiddleware');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 
+
+const checkAuth = require('../middlewares/authMiddleware');
+const checkRole = require('../middlewares/checkRole');
 /* GET users listing. */
 router.get('/',checkAuth, usersController.getUsers);
 router.get('/me',checkAuth, usersController.getUserByUid);
-
+router.put('/:uid/status', checkAuth , checkRole(['admin']), usersController.toggleUserStatus);
+router.put('/:uid/role', checkAuth, checkRole(['admin']), usersController.changeUserRole);
+router.delete('/:uid', checkAuth, checkRole(['admin']), usersController.deleteUser);
 module.exports = router;

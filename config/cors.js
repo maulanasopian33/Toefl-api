76a7768570440c3a67844b050ef615c03fd50f41
Maulana Sopian <maulana@antmediahost.com>
2025-11-26 10:33:10 +0700

    :hammer: fix cors

--- config/cors.js
+++ config/cors.js
@@ -3,7 +3,7 @@
 const corsOptions = {
   // Ganti 'http://localhost:3000' dengan URL front-end Anda
   // Anda juga bisa memasukkan beberapa URL dalam array
-  origin: ['http://localhost:3000','https://toafl.maulanasopian.my.id/'],
+  origin: ['http://localhost:3000','https://toafl.maulanasopian.my.idss'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };

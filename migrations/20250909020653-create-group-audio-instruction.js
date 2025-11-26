a07fa2da3016cc2f771db24560294d7bd08a8164
Maulana Sopian <maulana@antmediahost.com>
2025-11-26 14:20:04 +0700

    :hammer: update db

--- migrations/20250909020653-create-group-audio-instruction.js
+++ migrations/20250909020653-create-group-audio-instruction.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('groupAudioInstructions', {
+    await queryInterface.createTable('groupaudioinstructions', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('groupAudioInstructions');
+    await queryInterface.dropTable('groupaudioinstructions');
   }
 };

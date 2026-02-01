b2d2311c6172ed64818a0387b03f7d21421b68fe
Maulana Sopian <maulana@antmediahost.com>
2026-02-01 16:03:37 +0700

    chore: Disable the general request rate limiting middleware.

--- app.js
+++ app.js
@@ -54,14 +54,14 @@ app.use(helmet({
 
 // Rate Limiting
 const rateLimit = require('express-rate-limit');
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message: { status: false, message: 'Terlalu banyak permintaan, silakan coba lagi nanti.' }
-});
-app.use(limiter);
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+//   message: { status: false, message: 'Terlalu banyak permintaan, silakan coba lagi nanti.' }
+// });
+// app.use(limiter);
 
 // Specific limiter for login to prevent brute force
 const loginLimiter = rateLimit({
